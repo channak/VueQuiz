5448307d7de61cdc79d09bbd8b4667d199dc7be0
channakchhon <channak.chhon@passerellesnumeriques.org>
2019-10-23 09:13:43 +0700

    Change selected answer background color

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="question-box">
     <b-jumbotron>
-      <template v-slot:lead>
-        {{currentQuestion.question}}      
-      </template>
+      <template v-slot:lead>{{currentQuestion.question}}</template>
 
       <hr class="my-4" />
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+          :class="[selectedIndex===index?'list-group-item-primary':'']"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
 
-      <p v-for="(answer, index) in answers" :key="index">
-        {{answer}}
-      </p>
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button variant="success" href="#" @click="next">Next</b-button>
     </b-jumbotron>
@@ -18,16 +21,42 @@
 
 <script>
 export default {
-    props:{
-        currentQuestion:Object,
-        next:Function
-    },
-    computed: {
-        answers(){
-            let answers = [...this.currentQuestion.incorrect_answers]
-            answers.push(this.currentQuestion.correct_answer)
-            return answers
-        }
-    },
-}
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  }
+};
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.jumbotron {
+  padding: 0;
+}
+.red {
+  background: red;
+}
+</style>
